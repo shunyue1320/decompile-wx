22a5d3dfcd86aa730aeb82a3c9ff71bf680f97aa
shunyue1320 <1277952981@qq.com>
2021-02-02 23:34:05 +0800

    添加vue3

---
+++ src/view/pages/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div style="width: 100px; height: 100px; background: red;">拖拽到此执行</div>
+</template>
+<script>
+import { ref } from 'vue'
+import { doUnpack, convertTaro, convertUniapp } from '@/plugins/decompiler/wxapkg'
+
+export default {
+  setup() {
+    
+  }
+}
+</script>
+<style>
+</style>
