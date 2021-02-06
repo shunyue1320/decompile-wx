eb43288fcae5e2dd245c6b284f659c0e7883040d
shunyue1320 <1277952981@qq.com>
2021-02-06 23:32:18 +0800

    electron main

--- src/main/index.js
+++ src/main/index.js
@@ -1,88 +1,48 @@
-const electron = require('electron')
-const { app, BrowserWindow, globalShortcut, ipcMain } = require('electron')
-const fs = require("fs");
-const package = require("../../package.json");
-const bn = require('bytenode');
-const path = require('path');
-const userDataPath = app.getPath('userData');
+import path from 'path'
+import { app, BrowserWindow, Menu, screen, globalShortcut, ipcMain } from 'electron'
+import { doFile } from './decompiler/wxapkg'
 
-let mainWindow;
+const nodeEnv = process.env.NODE_ENV || 'development'
 
-//窗口对象
-var windowobj;
-var defaultSize = { width: 300, height: 300 };
-// if (process.platform == 'darwin') app.dock.hide();
-
-
-ipcMain.on("openDevTools", openDevTools);
-
-function createMainWindow(loadpath) {
-  const {
-    width,
-    height
-  } = electron.screen.getPrimaryDisplay().workAreaSize
-  windowobj = {
-    width: defaultSize.width,
-    height: defaultSize.height,
-    maximizable: false,
-    minimizable: false,
-    resizable: false,
-    fullscreenable: false,
-    frame: false,
-    transparent: true,
-    focusable: true,
-    hasShadow: false,
-    skipTaskbar: true,
-    backgroundColor: "#00ffffff",
-    // opacity:!config.get("transparent")?1:config.get("transparent"),
-    // movable:false,
-    show: false,
-    alwaysOnTop: true,
-    titleBarStyle: 'xxx',
+let mainWindow
+function createWindow() {
+  const area = screen.getPrimaryDisplay().workAreaSize
+  const minWidth = 800
+  const minHeight = 600
+  mainWindow = new BrowserWindow({
+    width: Math.max(parseInt(area.width * 0.7, 10), minWidth),
+    height: Math.max(parseInt(area.height * 0.9, 10), minHeight),
+    minWidth,
+    minHeight,
+    autoHideMenuBar: true,
+    icon:  path.join(__dirname, 'logo.ico'),
     webPreferences: {
-      devTools: true,
-      nodeIntegration: true,
-      webSecurity: false,
-      enableRemoteModule: true,
-      backgroundThrottling: false
-    }
-  }
-
-  // let lastBounds = config.get('winBounds');
-  // if(lastBounds)Object.assign(windowobj, {x:lastBounds.x,y:lastBounds.y});
-  mainWindow = new BrowserWindow(windowobj);
-  // mainWindow.webContents.openDevTools({mode:'detach',activate:true})
-  // mainWindow.loadURL(path.posix.join("file:",__dirname, loadpath?('engine3.html?model='+loadpath):'engine3.html'));
-  mainWindow.loadFile('index.html');
-  mainWindow.once("ready-to-show", function () {
-    mainWindow.show();
-  });
-  mainWindow.on('close', function () {});
-
-  mainWindow.on('closed', function () {
-    mainWindow = null;
+      nodeIntegration: true
+    },
+    title: '小程序反编译'
   })
 
-  globalShortcut.register('CommandOrControl+`', () => {});
+  ipcMain.on('decompiler-start', (event) => {
+    doFile("C:\\Users\\12779\\learning\\electron_app\\decompile-wx\\wxapkg\\跳一跳.wxapkg", ()=>{}, [])
+    event.sender.send('decompiler-success', { content: '反编译成功', path: '' })
+  })
+
+  if (nodeEnv === 'production') {
+    mainWindow.on('focus', (e) => {
+      globalShortcut.registerAll(['CommandOrControl+R','F5'], () => {})
+    })
+
+    mainWindow.on('blur', (e) => {
+      globalShortcut.unregisterAll()
+    })
+    mainWindow.loadFile('dist/index.html')
+  } else {
+    mainWindow.loadURL('http://127.0.0.1:8080')
+  }
 }
 
-function openDevTools() {
-  if (mainWindow) mainWindow.webContents.openDevTools({ mode: 'detach', activate: true })
+if (nodeEnv === 'production') {
+  Menu.setApplicationMenu(null)
 }
 
-// 限制只可以打开一个应用, 当运行第二个实例时退出第一个实例
-const gotTheLock = app.requestSingleInstanceLock()
-if (!gotTheLock) {
-  //app.quit()
-}
-app.on('second-instance', (event, commandLine, workingDirectory) => {
-  app.quit();
-})
-
-app.on('ready', function () {
-  createMainWindow();
-})
-app.on('window-all-closed', function () { })
-app.on('activate', function () { })
-app.on('browser-window-focus', function () { });
-app.on('browser-window-blur', function () { });
+app.on('ready', createWindow)
