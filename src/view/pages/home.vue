02ee166bfb82149a33740131a18b56ab468b29e0
shunyue1320 <1277952981@qq.com>
2021-02-06 23:32:12 +0800

    home page

--- src/view/pages/home.vue
+++ src/view/pages/home.vue
@@ -1,15 +1,31 @@
 <template>
-  <div style="width: 100px; height: 100px; background: red;">拖拽到此执行</div>
+  <div @ondrop="convertWxapkg" style="width: 100px; height: 100px; background: red;">拖拽到此执行Wxapkg</div>
 </template>
 <script>
 import { ref } from 'vue'
-import { doUnpack, convertTaro, convertUniapp } from '@/plugins/decompiler/wxapkg'
+import { shell, ipcRenderer } from 'electron'
 
 export default {
   setup() {
-    
+    const content = shallowRef('')
+
+    function convertWxapkg(event) {
+      e.preventDefault();
+      let fileName = e.dataTransfer.files[0].path;
+      if (fileName.endsWith('.wxapkg')) {
+        ipcRenderer.send('decompiler-start', )
+        ipcRenderer.on('decompiler-success', (e, arg) => {
+          content.value = arg.content
+          shell.openItem(arg.path)
+        })
+      } else {
+        alert("文件格式错误，应为 '.wxapkg' 结尾")
+      }
+    }
+    return {
+      content,
+      convertWxapkg
+    }
   }
 }
 </script>
-<style>
-</style>
