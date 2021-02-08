6d26917172d770d698afb919fa21f6ae3b6c6226
shunyue1320 <1277952981@qq.com>
2021-02-09 00:25:03 +0800

    add ant-design-vue

--- src/view/main.js
+++ src/view/main.js
@@ -1,9 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { Button, Row, Col, Tooltip } from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css';
 
 const app = createApp(App)
 
+app.use(Button)
+app.use(Row)
+app.use(Col)
+app.use(Tooltip)
 app.use(router)
 
 app.mount('#app')
